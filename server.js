dda48d3b43aaf47063ee7836dd9a508ca8733cc4
b1gvini <vgf.vinicius@gmail.com>
2020-02-19 11:24:03 -0300

    FINAlIZADO!!

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ server.use(express.static('public'))
 
 server.use(express.urlencoded({ extended: true}))
 
+//configurar a conexão com o bd
+const Pool = require('pg').Pool
+const db = new Pool({
+    user: 'postgres',
+    password: 'root',
+    host: 'localhost',
+    port: 5432,
+    database: 'doe'
+})
+
+
 //configurando o template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("./",{
@@ -18,35 +29,16 @@ nunjucks.configure("./",{
 })
 
 
-// Lista de doadores.
-const donors = [
-    {
-        name: "Vinícius de França",
-        blood: "A-"
-    },
-    {
-        name: "Fernanda Lira",
-        blood: "O+"
-
-    },
-    {
-        name: "Lucas Ranniery",
-        blood: "AB+"
-    },
-    {
-        name: "David Amorin",
-        blood: "O-"
-    }
-
-
-
-]
-
-
-
 //configurar a apresentação da página
 server.get("/",function(req, res){
-    return res.render("index.html", { donors })
+    db.query("SELECT * from donors", function(err, result){
+        if(err) return res.send("Erro de exibição no Banco de Dados.")
+        
+
+        const donors = result.rows;
+        return res.render("index.html", { donors })
+    })
+    
 })
 
 server.post("/", function(req, res){
@@ -55,13 +47,24 @@ server.post("/", function(req, res){
     const email = req.body.email
     const blood = req.body.blood
 
-    //colocando valores dentro do array
-    donors.push({
-        name: name,
-        blood: blood,
-    })
+    if (name == "" || email == "" || blood == ""){
+        return res.send("Todos os campos são obrigatórios.")
+    }
 
-    return res.redirect("/")
+    //colocando valores dentro do bd
+    const query = `INSERT INTO donors("name","email","blood") 
+    VALUES ($1, $2, $3)`
+
+    const values = [name, email, blood]
+    db.query(query, values, function(err){
+        //fluxo de erro
+        if (err) return res.send("Erro no Banco de Dados.")
+        
+        //fluxo ideal
+        return res.redirect("/")
+    } )
+
+    
 })
 
 
